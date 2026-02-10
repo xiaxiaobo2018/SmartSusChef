1ae789bfb6ca846c40b00a37ab1bfbca0567929e
z030511tnt-pixel <z030511tnt@gmail.com>
2026-02-10 12:59:29 +0800

    Datatest

---
+++ frontend/playwright_e2e/data/export.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '@playwright/test';
+
+test('Export Data Test', async ({ page }) => {
+    await page.goto('http://localhost:5173/login');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Simon');
+    await page.getByRole('textbox', { name: 'Password' }).fill('Leinuozhen2003.');
+    await page.getByRole('button', { name: 'Sign In' }).click();
+    await page.getByRole('button', { name: 'Manage Store' }).click();
+    await page.getByRole('button', { name: 'Export Data' }).click();
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'Export as CSV' }).first().click();
+    const download = await downloadPromise;
+    expect(await download.failure()).toBeNull();
+    await download.saveAs('./downloads/exported_data.csv');
+    console.log(`下载成功，文件名: ${download.suggestedFilename()}`);
+});
